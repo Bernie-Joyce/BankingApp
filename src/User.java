e11b7efa842e746b40927884090fa495fb63b08d
Bernard_Joyce <joycebernie833@gmail.com>
2025-07-12 19:55:11 +0100

    update to add attributes and constructors

--- src/User.java
+++ src/User.java
@@ -1,2 +1,27 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+
 public class User {
+    private int pin;
+    private double balance;
+    private LocalDate dateOfBirth;
+    private String name;
+
+    public User(int pin,double balance,LocalDate dateOfBirth,String name){
+        this.pin=pin;
+        this.balance=balance;
+        this.dateOfBirth=dateOfBirth;
+        this.name=name;
+    }
+
+    public User(){
+        pin = -1;
+        balance = -1;
+        dateOfBirth = LocalDate.MIN;
+        name = "";
+    }
+
+
+
 }
